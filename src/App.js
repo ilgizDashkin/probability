12abc0b620c44712dfe1378ce70b5a0c790abc77
ilgizDashkin <48960359+ilgizDashkin@users.noreply.github.com>
2020-03-06 15:38:48 +0500

    улучшил внешний вид, добавил формулы

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ function App() {
 
   return (
     <div className="container p-2   text-white p-3 bg-dark" >
-      <Panel name="простая вероятность" id_1='simple_probality1' id_2='simple_probality2' id_3='result_simple_probality'
-      name_input_1="количество положительных исходов" name_input_2="количество всех возможных исходов" set_result={simple_probality} />
+      <Panel name="простая вероятность (=m/n)" id_1='simple_probality1' id_2='simple_probality2' id_3='result_simple_probality'
+      name_input_1="количество положительных исходов n" name_input_2="количество всех возможных исходов m" set_result={simple_probality} />
 
-      <Panel name="комбинаторика размещение" id_1='combinat_placement1' id_2='combinat_placement2' id_3='result_combinat_placement'
-      name_input_1="количество различных элементов" name_input_2="количество мест возможных размещений этих элементов"set_result={combinat_placement} />
+      <Panel name="комбинаторика размещение ( =n!/(n-m)! )" id_1='combinat_placement1' id_2='combinat_placement2' id_3='result_combinat_placement'
+      name_input_1="количество различных элементов m" name_input_2="количество мест возможных размещений этих элементов n"set_result={combinat_placement} />
 
-     <Panel name="комбинаторика сочетание" id_1='combinat_combination1' id_2='combinat_combination2' id_3='result_combinat_combination'
-      name_input_1="количество различных элементов" name_input_2="количество мест возможных размещений этих элементов"set_result={combinat_combination} />
+     <Panel name="комбинаторика сочетание ( =n!/m!(n-m)! )" id_1='combinat_combination1' id_2='combinat_combination2' id_3='result_combinat_combination'
+      name_input_1="количество различных элементов m" name_input_2="количество мест возможных размещений этих элементов n"set_result={combinat_combination} />
 
-    <Panel name="комбинаторика размещение" id_1='combinat_transposition1' id_2='combinat_transposition2' id_3='result_combinat_transposition'
-      name_input_1="количество различных элементов соответствует количеству мест (вводить не надо)" name_input_2="количество мест возможных размещений этих элементов"set_result={combinat_transposition} />
+    <Panel name="комбинаторика перестановка (подстановка) ( =n!)" id_1='combinat_transposition1' id_2='combinat_transposition2' id_3='result_combinat_transposition'
+      name_input_1="количество различных элементов соответствует количеству мест (вводить не надо)" name_input_2="количество мест возможных перестановок этих элементов n"set_result={combinat_transposition} />
     
     </div>
   );
